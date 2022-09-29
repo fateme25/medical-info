37cba8f1e92df4dae76db445f0545d74fdb7dd98
Fatemeh25 <79019952+fatmeh25@users.noreply.github.com>
2022-09-29 17:48:42 +0330

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+const menuTrigger = document.getElementById('menu-trigger');
+const navUL = document.querySelector(".links");
+const hamburger = document.querySelector('.menu-trigger');
+
+menuTrigger.addEventListener('click', () => {
+  navUL.classList.toggle('show');
+  hamburger.classList.toggle('show');
+
+});
+
+// dropdown 
+$(document).ready(function dropDown() {
+  $('.fa').click(function () {
+    $(this).closest('.dropdown').find('.dropdown-content').toggle();
+  })
+});
+
+
+//News Api 
+const newsList = document.getElementById("news-list");
+let healthyNews = [];
+async function loadNews() {
+  try {
+    const news = await fetch("https://health.gov/myhealthfinder/api/v3/myhealthfinder.json");
+    healthyNews = await news.json();
+    let data = healthyNews.Result.Resources.all.Resource;
+    // console.log(data)
+    displayNews(data);
+  }
+  catch (err) {
+    console.log(err);
+  }
+}
+
+function displayNews(information) {
+  const desc = information.map((topic) => {
+    return `
+        <li>
+         <h3>${topic.Title}</h3>
+        <p>${topic.MyHFDescription}</p>
+        <a href="${topic.AccessibleVersion}">Read more</a>
+        </li>`
+  }).join('');
+  newsList.innerHTML = desc;
+}
+
+loadNews();
